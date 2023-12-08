a447bf0c313e52fbf272884d622a6568ff8b60a1
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-12-08 14:48:39 -0800

    Expand placholder component and add much more sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Nav from "components/Nav";
 import StandardPlaceholder from "components/StandardPlaceholder";
 
 function App() {
@@ -9,9 +6,53 @@ function App() {
     <div className="App">
       {/* <img src="images/nav.png" alt="navigation" />
       <img src="images/LandingScreen.png" alt="navigation" /> */}
-      <StandardPlaceholder src="images/nav.png" link="#" />
-      <StandardPlaceholder src="images/LandingScreen.png" link="#" />
+      <StandardPlaceholder
+        src="images/nav.png"
+        srcMob="images/NavMob.png"
+        link="#"
+      />
+      <StandardPlaceholder
+        src="images/LandingScreen.png"
+        srcMob="images/LandingScreenMob.png"
+        link="#"
+      />
       <StandardPlaceholder src="images/PivitalContent.png" link="#" />
+      <StandardPlaceholder src="images/PSAGLanding.png" link="#" />
+      <StandardPlaceholder src="images/PSAGDetailedDisplay.png" link="#" />
+      <StandardPlaceholder src="images/PSAGSlides.png" link="#" />
+      <StandardPlaceholder src="images/CapyLanding.png" link="#" />
+      <StandardPlaceholder src="images/CapyDetailedDisplay.png" link="#" />
+      <StandardPlaceholder src="images/CapySlides.png" link="#" />
+      <StandardPlaceholder src="images/SpongeLanding.png" link="#" />
+      <StandardPlaceholder src="images/SpongeDetailedDisplay.png" link="#" />
+      <StandardPlaceholder src="images/SpongeSlides.png" link="#" />
+      <StandardPlaceholder src="images/ChimpLanding.png" link="#" />
+      <StandardPlaceholder src="images/ChimpDetailedDisplay.png" link="#" />
+      <StandardPlaceholder src="images/ChimpSlides.png" link="#" />
+      <StandardPlaceholder src="images/GravityLanding.png" link="#" />
+      <StandardPlaceholder src="images/GravityDetailedDisplay.png" link="#" />
+      <StandardPlaceholder src="images/GravitySlides.png" link="#" />
+      <StandardPlaceholder src="images/JoinNewsLetter.png" link="#" />
+      <StandardPlaceholder src="images/ContactMeTitle.png" link="#" />
+      <StandardPlaceholder
+        src="images/ContactMeContentLeft.png"
+        src2="images/ContactMeContentRight.png"
+        link="#"
+        link2="#"
+        settings={{ dskSplit: true }}
+      />
+      <StandardPlaceholder src="images/ScheduleInterview.png" link="#" />
+      <StandardPlaceholder src="images/Transition.png" link="#" />
+      <StandardPlaceholder src="images/DownloadResume.png" link="#" />
+      <StandardPlaceholder
+        src="images/Card1.png"
+        src2="images/Card2.png"
+        link="#"
+        link2="#"
+        settings={{ dskSplit: true }}
+      />
+      <StandardPlaceholder src="images/ContactInfoCore.png" link="#" />
+      <StandardPlaceholder src="images/PoweredBy.png" link="#" />
     </div>
   );
 }
--- src/components/StandardPlaceholder/StandardPlaceholder.tsx
+++ src/components/StandardPlaceholder/StandardPlaceholder.tsx
@@ -1,11 +1,33 @@
 import "./standardPlaceholder.css";
+import useIsMobile from "components/useIsMobile";
 
-function StandardPlaceholder({ src, link }: any) {
+function StandardPlaceholder({
+  src,
+  srcMob,
+  link,
+  linkMob,
+  src2,
+  link2,
+  settings,
+}: any) {
+  const { dskSplit }: any = settings || {};
+  const isMobile: boolean = useIsMobile();
+
+  const splitForDsk = !isMobile && dskSplit;
+
+  const trueSrc = isMobile ? srcMob || src : src;
   return (
-    <div className="standard_placeholder_container">
-      <a href={link}>
-        <img src={src} alt="navigation" />
+    <div
+      className={`standard_placeholder_container ${splitForDsk ? "split" : ""}`}
+    >
+      <a href={link} className={`${splitForDsk ? "split" : ""}`}>
+        <img src={trueSrc} alt="navigation" />
       </a>
+      {src2 && (
+        <a href={link2} className="split">
+          <img src={src2} alt="navigation" />
+        </a>
+      )}
     </div>
   );
 }
